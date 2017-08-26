fb5f4e2058dfdadbb39e7970835a3ddcba2c3275
Pratik Upacharya <pratikupacharya@gmail.com>
2017-08-26 21:57:03 +0530

    Basic Setup is done

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-console.log('Testing Package');
+'use strict'
+let jsonfile = require('jsonfile');
+
+let file = process.cwd() + '/package.json';
+
+const exec = require('child_process').exec;
+
+jsonfile.readFile(file, function(err, obj) {
+    let command = obj.shortcutjs[process.argv[2]];
+
+    const child = exec(command,
+        (error, stdout, stderr) => {
+            console.log(`stdout: ${stdout}`);
+            console.log(`stderr: ${stderr}`);
+            if (error !== null) {
+                console.log(`exec error: ${error}`);
+            }
+        });
+})
