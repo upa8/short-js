25c03f6314472276ea54b476a024511fbeda51ea
Pratik Upacharya <pratikupacharya@gmail.com>
2017-08-26 22:03:04 +0530

    Check for command

--- index.js
+++ index.js
@@ -5,15 +5,25 @@ let file = process.cwd() + '/package.json';
 
 const exec = require('child_process').exec;
 
-jsonfile.readFile(file, function(err, obj) {
-    let command = obj.shortcutjs[process.argv[2]];
+if( process.argv[2] != null){
+    jsonfile.readFile(file, function(err, obj) {
+        if( obj.shortcutjs[process.argv[2]] != null){
+            let command = obj.shortcutjs[process.argv[2]];
 
-    const child = exec(command,
-        (error, stdout, stderr) => {
-            console.log(`stdout: ${stdout}`);
-            console.log(`stderr: ${stderr}`);
-            if (error !== null) {
-                console.log(`exec error: ${error}`);
-            }
-        });
-})
+            const child = exec(command,
+                (error, stdout, stderr) => {
+
+                    console.log(`${stdout}`);
+                    console.log(`${stderr}`);
+
+                    if (error !== null) {
+                        console.log(`exec error: ${error}`);
+                    }
+                });
+        }else{
+            console.log('Command does not exits');
+        }
+    })
+}else{
+    console.log('Please enter valid second parameter');
+}
