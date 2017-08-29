f614b486fe0dfa1956338d4acd221197e6969aa1
Pratik Upacharya <pratikupacharya@gmail.com>
2017-08-29 10:51:01 +0530

    Added ESlint

--- index.js
+++ index.js
@@ -1,30 +1,30 @@
 #!/usr/bin/env node
-'use strict'
-let jsonfile = require('jsonfile');
 
-let file = process.cwd() + '/package.json';
+const jsonfile = require('jsonfile');
 
 const exec = require('child_process').exec;
 
-if( process.argv[2] != null){
-    jsonfile.readFile(file, function(err, obj) {
-        if( obj.shortcutjs[process.argv[2]] != null){
-            let command = obj.shortcutjs[process.argv[2]];
+const file = `${process.cwd()}/package.json`;
 
-            const child = exec(command,
-                (error, stdout, stderr) => {
 
-                    console.log(`${stdout}`);
-                    console.log(`${stderr}`);
+if (process.argv[2] != null) {
+  jsonfile.readFile(file, (err, obj) => {
+    if (obj.shortcutjs[process.argv[2]] != null) {
+      const command = obj.shortcutjs[process.argv[2]];
 
-                    if (error !== null) {
-                        console.log(`exec error: ${error}`);
-                    }
-                });
-        }else{
-            console.log('Command does not exits');
-        }
-    })
-}else{
-    console.log('Please enter valid second parameter');
+      exec(command,
+        (error, stdout, stderr) => {
+          console.log(`${stdout}`);
+          console.log(`${stderr}`);
+
+          if (error !== null) {
+            console.log(`exec error: ${error}`);
+          }
+        });
+    } else {
+      console.log('Command does not exits');
+    }
+  });
+} else {
+  console.log('Please enter valid second parameter');
 }
